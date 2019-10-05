9f8ab2bf464f76f86d548915b84aabedab39b63b
Douglas Wilhelm <dwilhelm@vacayhomeconnect.com>
2019-10-04 23:10:02 -0500

    Clean up formatting in Chooser component, add conditional REST API prefix

--- client/src/components/Chooser/Chooser.tsx
+++ client/src/components/Chooser/Chooser.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { TimeSlot } from '../../models/timeSlot';
 
 const Chooser = (): React.ReactElement => {
+  const fetchUrlPrefix = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '';
   const [ timeSlots, setTimeSlots ] = useState([]);
   const [ loading, setLoading ] = useState(true);
   const [ error, setError ] = useState(null);
 
   async function getTimeSlots() {
-      const res = await fetch('/employer_schedules');
+      const res = await fetch(`${fetchUrlPrefix}/employer_schedules`);
       res
         .json()
         .then(res => {
@@ -34,7 +35,9 @@ const Chooser = (): React.ReactElement => {
   return (
     <div>
       <p>Select a date and time</p>
-      { timeSlots.length ? timeSlots.map((timeSlot: TimeSlot) => <p>ID: {timeSlot.id} - Start time: {timeSlot.start_time}</p>) : <p>No time slots available.</p>}
+      { timeSlots.length ? timeSlots.map((timeSlot: TimeSlot, index) =>
+        <p key={index}>ID: {timeSlot.id} - Start time: {timeSlot.start_time}</p>) :
+        <p>No time slots available.</p>}
     </div>
   )
 };
